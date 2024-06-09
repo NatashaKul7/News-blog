0a7227aa0c9e6e73bbdd13d8f215ef4f46f4856e
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2024-06-09 16:42:59 +0200

    +

--- components/modal-backdrop.js
+++ components/modal-backdrop.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { notFound, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function ModalBackdrop() {
   const router = useRouter();
